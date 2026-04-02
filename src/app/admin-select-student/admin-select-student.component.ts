ddcab0a818f98545fe1f2f4c261a6bb6902b0a93
Samantha Gayle Oliveros <gayleoliveros2016@gmail.com>
2026-04-02 23:00:08 +0800

    Made the student names clickable to go next page

---
+++ src/app/admin-select-student/admin-select-student.component.ts
@@ -0,0 +1,27 @@
+import { CommonModule, Location } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+    selector: 'app-admin-select-student',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './admin-select-student.component.html',
+    styleUrl: './admin-select-student.component.scss'
+})
+export class AdminSelectStudentComponent implements OnInit {
+    studentName: string | null = '';
+
+    constructor(
+    private route: ActivatedRoute, 
+    private location: Location
+    ) {}
+
+    ngOnInit(): void {
+    this.studentName = this.route.snapshot.paramMap.get('id');
+    }
+
+    goBack(): void {
+    this.location.back();
+    }
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { AdminProfileComponent } from './admin-profile/admin-profile.component';
+import { AdminSelectStudentComponent } from './admin-select-student/admin-select-student.component';
 import { AdminSidebarComponent } from './admin-sidebar/admin-sidebar.component';
 import { StudentListComponent } from './admin-student-list/admin-student-list.component';
 import { LoginComponent } from './login/login.component';
@@ -9,5 +10,6 @@ export const routes: Routes = [
   { path: '', component: LoginComponent }, // default route
   { path: 'admin-students-list', component: StudentListComponent },
   { path: 'admin-profile', component: AdminProfileComponent }, //admin profile
-  { path: 'admin', component: AdminSidebarComponent} // path for the sidebar
+  { path: 'admin', component: AdminSidebarComponent}, // path for the sidebar
+  { path: 'admin/student-profile/:id', component: AdminSelectStudentComponent}
 ];
