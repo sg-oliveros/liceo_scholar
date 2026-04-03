a6e986db3d6f73dd1ab21bae7986441a06cbe824
Christy Reyes <careyes04249@liceo.edu.ph>
2026-04-03 20:11:05 +0800

    fixed scss

--- src/app/student-profile/student-profile.component.ts
+++ src/app/student-profile/student-profile.component.ts
@@ -3,26 +3,31 @@ import { Component, OnInit } from '@angular/core';
 import { UserSidebarComponent } from '../user-sidebar/user-sidebar.component';
 
 @Component({
-    selector: 'app-student-profile',
-    standalone: true,
-    imports: [CommonModule, UserSidebarComponent],
-    templateUrl: './student-profile.component.html',
-    styleUrls: ['./student-profile.component.scss']
+  selector: 'app-student-profile',
+  standalone: true,
+  imports: [CommonModule, UserSidebarComponent],
+  templateUrl: './student-profile.component.html',
+  styleUrls: ['./student-profile.component.scss']
 })
 export class StudentProfileComponent implements OnInit {
 
-    student = {
-        fullName: 'OLIVEROS, SAMANTHA GAYLE R.',
-        course: 'CISCO BSIT',
-        yearLevel: '2ND YEAR',
-        age: '18 YEARS OLD',
-        sex: 'FEMALE',
-        scholarshipType: 'ACADEMIC SCHOLARSHIP (FULL SCHOLAR)',
-        profileImage: 'assets/profile.jpg',  // put your image here
-        
-    };
+  student = {
+    fullName: 'OLIVEROS, SAMANTHA GAYLE R.',
+    course: 'CISCO BSIT',
+    yearLevel: '2ND YEAR',
+    age: '18 YEARS OLD',
+    sex: 'FEMALE',
+    scholarshipType: 'ACADEMIC SCHOLARSHIP (FULL SCHOLAR)',
+    profileImage: 'assets/images/profile.jpg',
+    history: [
+      { term: '3RD YR (1ST SEMESTER)', status: 'PENDING' },
+      { term: '2ND YR (2ND SEMESTER)', status: 'FINISHED' },
+      { term: '2ND YR (1ST SEMESTER)', status: 'FINISHED' },
+      { term: '1ST YR (2ND SEMESTER)', status: 'FINISHED' },
+      { term: '1ST YR (1ST SEMESTER)', status: 'FINISHED' },
+    ]
+  };
 
-    constructor() { }
-
-    ngOnInit(): void { }
+  constructor() { }
+  ngOnInit(): void { }
 }
